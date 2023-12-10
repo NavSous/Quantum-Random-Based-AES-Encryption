d0fa6e1da8a75faaaa3006d6e09f1624a9b41672
NavSous <navhs360@gmail.com>
2023-12-10 14:04:54 -0800

    Initialize

---
+++ qrand.py
@@ -0,0 +1,28 @@
+import cirq
+import numpy as np
+import random
+from cryptography.fernet import Fernet
+
+class QRNG():
+    def __init__(self, simulator=None):
+        if simulator is None:
+            simulator = cirq.Simulator()
+        self.simulator=simulator
+    def generate_binary_array(self, length, showsteps):
+        #Initialize Qubit Array
+        qubits = cirq.NamedQubit.range(length, prefix="qnum_")
+        #Randomization using the Hadamard Gate: https://www.quantum-inspire.com/kbase/hadamard/. 1/sqrt(2)
+        hadamard_gate = [cirq.H(qubit) for qubit in qubits]
+        #Measure Results
+        measurement_gate = cirq.measure(qubits, key = "qrng_measure")
+        #Create circuit
+        qrng_circuit = cirq.Circuit(hadamard_gate, measurement_gate)        
+        #Show work or not
+        simulator = self.simulator
+        if showsteps:
+            for step, i in enumerate(simulator.simulate_moment_steps(qrng_circuit)):
+                print(f'Step: {step}, State:{i.state_vector()}' )
+        #Simulate Quantum Env
+        result = simulator.simulate(qrng_circuit)
+        #print(result.measurements['qrng_measure'].tostring())
+        return result.measurements['qrng_measure'].tostring()
